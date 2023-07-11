12216b627907f73d6c76e6b635f62ade728e0bd9
chasespengler <chasespengler7@gmail.com>
2023-07-11 16:06:46 -0400

    footer sizing change

--- chasev2-frontent/src/components/Footer/index.js
+++ chasev2-frontent/src/components/Footer/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function Foot() {
   return (
-    <div id="outer-foot" style={{"background": "linear-gradient(90deg, #113137 0%, white 30%)"}}>
+    <div id="outer-foot" style={{"background": "linear-gradient(90deg, #113137 0%, white 30%)", "max-height": "125px"}}>
       <div className="container-fluid" id="foot">
         <footer className="panel-footer">
           <div className="text-center" id="footer-text">
             <span>
-              <img style={{"maxHeight": "45px"}} src={require("../../images/CS.png")} alt="logo" className="logo" id="footer-logo"/>
+              <img style={{"maxHeight": "45px"}} src={require("../../images/CS.png")} alt="logo" id="footer-logo"/>
               Website designed by and property of Chase Spengler.
             </span>
           </div>
