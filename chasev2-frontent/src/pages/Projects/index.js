2c0ebb0017cd03296b6679e77bc83cd125430ab7
chasespengler <chasespengler7@gmail.com>
2023-11-07 13:12:16 -0500

    todo comment added, link change on project page, spacing on project page

--- chasev2-frontent/src/pages/Projects/index.js
+++ chasev2-frontent/src/pages/Projects/index.js
@@ -11,25 +11,34 @@ import {
     ProjectLink,
 } from './ProjectsElements'
 
+// TODO: Add 'click-here' key/pair to dater and add a last sentence in the summary that changes from the home page to here
+// eg 'Click here to view the project on github' with link to github vs 'Click here to view more' with link to project specific page
+
+
 export default function ProjectsPage () {
 
     const projects = dater['projects']
 
     return (
-        <ProjectsContainer>
-            <Row style={{paddingRight: '40px'}}>
-                {projects.map((project, index) => (
-                    <Col xs={6}>
-                        <ProjectPageCard>
-                            <ProjectImg src={require("../../images/"+project['img_src'])} className="card-img-top img-fluid" />
-                            <ProjectBlock>
-                                <ProjectTitle className={'card-title'}>{project['title']}</ProjectTitle>
-                                <ProjectSummary>{project['summary']}</ProjectSummary>
-                                <ProjectLink href={'/#/projects/project/'+project['id']} className={"stretched-link"}></ProjectLink>
-                            </ProjectBlock>
-                        </ProjectPageCard>
-                    </Col>
-                ))}
+        <ProjectsContainer style={{paddingBottom: '30px'}}>
+            <Row style={{display: 'flex', justifyContent: 'center', paddingBottom: '20px'}}>
+                <Col xs={12} xxl={6}>
+                    <Row style={{paddingRight: '40px'}}>
+                        {projects.map((project, index) => (
+                            <Col xs={12} md={6}>
+                                <ProjectPageCard style={{minHeight: '95%'}}>
+                                    <ProjectImg src={require("../../images/"+project['img_src'])} className="card-img-top img-fluid" />
+                                    <ProjectBlock>
+                                        <ProjectTitle className={'card-title text-center'}>{project['title']}</ProjectTitle>
+                                        <ProjectSummary>{project['summary']}</ProjectSummary>
+                                        <ProjectLink href={project['link']} target='_blank' className={"stretched-link"}></ProjectLink>
+                                        {/* <ProjectLink href={'/#/projects/project/'+project['id']} className={"stretched-link"}></ProjectLink> */}
+                                    </ProjectBlock>
+                                </ProjectPageCard>
+                            </Col>
+                        ))}
+                    </Row>
+                </Col>
             </Row>
         </ProjectsContainer>
     )
